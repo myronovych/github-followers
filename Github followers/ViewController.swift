0af13fa6c056ac6930d1e37ea1cfd5103856704b
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-07-23 10:21:22 +0300

    init

---
+++ Github followers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github followers
+//
+//  Created by rs on 23.07.2020.
+//  Copyright © 2020 Oleksandr Myronovych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
