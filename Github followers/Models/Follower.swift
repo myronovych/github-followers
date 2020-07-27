4e936b206c1461d774fd7b8e2b44004dfcfd958a
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-07-27 11:41:35 +0300

    added follower, user models. restructured project

---
+++ Github followers/Models/Follower.swift
@@ -0,0 +1,15 @@
+//
+//  Follower.swift
+//  Github followers
+//
+//  Created by rs on 27.07.2020.
+//  Copyright © 2020 Oleksandr Myronovych. All rights reserved.
+//
+
+import Foundation
+
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ Github followers/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Github followers
+//
+//  Created by rs on 27.07.2020.
+//  Copyright © 2020 Oleksandr Myronovych. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var followers: Int
+    var following: Int
+    var createdAt: String
+}
