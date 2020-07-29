e514946e9118f2e81a41febd82d0434181538dce
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-07-29 15:30:01 +0300

    custom avatar image view

--- Github followers/Custom Views/GFAvatarImageView.swift
+++ Github followers/Custom Views/GFAvatarImageView.swift
@@ -10,12 +10,23 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let placeholder = UIImage(named: "avatar-placeholder")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        layer.cornerRadius = 20
+        clipsToBounds = true
+        image = placeholder
+    }
+    
+    
 }
