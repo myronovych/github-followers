169f8a5ce1da0c086d04a3556e596eb2d3ffa48e
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-07-29 15:30:23 +0300

    Diffable data source implemented

--- Github followers/Screens/FollowersListViewController.swift
+++ Github followers/Screens/FollowersListViewController.swift
@@ -10,17 +10,59 @@ import UIKit
 
 class FollowersListViewController: UIViewController {
     
+    enum Section {
+        case main
+    }
+    
     var username: String!
+    var followers: [Follower]!
+    var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, Follower>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.prefersLargeTitles = true
-        view.backgroundColor = .systemBackground
         
+        configureCollectionView()
+        getFollowers()
+        configureDataSource()
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnsLayout())
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        collectionView.register(GFFollowerCollectionViewCell.self, forCellWithReuseIdentifier: GFFollowerCollectionViewCell.cellID)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    private func createThreeColumnsLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumSpaceBetweenColumns: CGFloat = 10
+        let availableSpace = width - (padding * 2) - (minimumSpaceBetweenColumns * 2)
+        let widthForCell = availableSpace / 3
+        
+        layout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        layout.itemSize = CGSize(width: widthForCell, height: widthForCell + 38)
+        
+        return layout
+    }
+
+    fileprivate func getFollowers() {
         NetworkManager.shared.getFollowers(username: username, page: 1) { result in
             
             switch result {
             case .success(let followers):
+                self.followers = followers
+                self.updateData()
                 print(followers)
                 
             case .failure(let error):
@@ -31,12 +73,21 @@ class FollowersListViewController: UIViewController {
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        navigationController?.setNavigationBarHidden(false, animated: true)
-        
+    func configureDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GFFollowerCollectionViewCell.cellID, for: indexPath) as! GFFollowerCollectionViewCell
+            cell.set(follower: follower)
+            
+            return cell
+        })
     }
     
+    func updateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Follower>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(followers)
+        dataSource.apply(snapshot, animatingDifferences: true)
+        
+    }
     
 }
